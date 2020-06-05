cd4104b9df4544613cb94b0ee166b79c76d86756
atlasmoth <ebuka422@gmail.com>
2020-06-05 21:33:01 +0100

    completed basic painting app

--- paint.js
+++ paint.js
@@ -1,30 +1,69 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-const { height, width } = canvas;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-// random colour generator
+// // random colour generator
+// const {height,width} = canvas
+// function randColor() {
+//   //
+//   const rand = () => Math.floor(Math.random() * 266);
+//   return `rgb(${rand()},${rand()},${rand()})`;
+// }
+
+// // random circle generator
+// function randCircle(num = 100) {
+//   const radius = () => Math.floor((Math.random() * width) / 8) + 5;
+//   const x = () => {
+//     return Math.floor(Math.random() * width - width / 8);
+//   };
+//   const y = () => {
+//     return Math.floor(Math.random() * height - height / 8);
+//   };
+
+//   for (let count = 0; count < num; count++) {
+//     ctx.beginPath();
+//     ctx.arc(x(), y(), radius(), 0, Math.PI * 2);
+//     ctx.strokeStyle = randColor();
+//     ctx.stroke();
+//   }
+// }
+
+// randCircle();
+
+let painting = false;
 function randColor() {
   //
   const rand = () => Math.floor(Math.random() * 266);
   return `rgb(${rand()},${rand()},${rand()})`;
 }
 
-// random circle generator
-function randCircle(num = 10000) {
-  const radius = () => Math.floor((Math.random() * width) / 8) + 5;
-  const x = () => {
-    return Math.floor(Math.random() * width - width / 8);
-  };
-  const y = () => {
-    return Math.floor(Math.random() * height - height / 8);
-  };
+function startPosition() {
+  painting = true;
+}
 
-  for (let count = 0; count < num; count++) {
-    ctx.beginPath();
-    ctx.arc(x(), y(), radius(), 0, Math.PI * 2);
+function endPosition() {
+  painting = false;
+  ctx.beginPath();
+}
+
+// event listeners
+// start painting
+canvas.addEventListener("mousedown", (e) => {
+  startPosition();
+});
+// end painting
+canvas.addEventListener("mouseup", (e) => {
+  endPosition();
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if (painting) {
+    ctx.lineWidth = 10;
+    ctx.lineCap = "round";
+    // ctx.moveTo(e.clientX, e.clientY);
+    ctx.lineTo(e.clientX, e.clientY);
     ctx.strokeStyle = randColor();
     ctx.stroke();
   }
-}
-
-randCircle();
+});
